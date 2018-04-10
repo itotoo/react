c15a74328657b21a490e25941cf5e474f7d3c5bf
liushengjie <liushengjie@mistong.com>
2018-04-10 15:42:44 +0800

    'react'

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-example
+官方文档
+https://doc.react-china.org/docs/hello-world.html
---
+++ readme.mk
@@ -0,0 +1,23 @@
+配置
+	安装node
+
+	执行npm i
+
+启动
+	cd gulpfile 目录 ，执行 gulp
+
+	
+目录强制规范
+	-common 公共文件
+		-js 
+		-css 
+	-plugin 插件
+	-images
+		file 添加你的项目文件夹
+		icon 公共图标
+	-js
+		file 添加你的项目文件夹
+	-scss
+		file 添加你的项目文件夹
+
+	.html
